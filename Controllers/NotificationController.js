e4e8ee701e730936345be65ba5542a46fccd407b
deshini kanchana <deshinisampath@gmail.com>
2025-07-28 12:45:33 +0530

    Few Changes Added In Notification Service

--- Controllers/NotificationController.js
+++ Controllers/NotificationController.js
@@ -1,33 +1,5 @@
-// const EventHub = require('../Models/EventHub');
-//
-// exports.registerListener = async (req, res) => {
-//     try {
-//         const { callback } = req.body;
-//         const existing = await EventHub.findOne({ callback });
-//         if (existing) return res.status(409).json({ message: "Already registered" });
-//
-//         const hub = await EventHub.create({ callback });
-//         res.status(201).location(`/hub/${hub._id}`).json({ id: hub._id, callback: hub.callback });
-//     } catch (err) {
-//         res.status(400).json({ error: err.message });
-//     }
-// };
-//
-// exports.unregisterListener = async (req, res) => {
-//     try {
-//         const { id } = req.params;
-//         await EventHub.findByIdAndDelete(id);
-//         res.status(204).end();
-//     } catch (err) {
-//         res.status(404).json({ error: "Hub not found" });
-//     }
-// };
-
-
-// controllers/notificationController.js
 const EventHub = require('../Models/EventHub');
 
-// Register a new listener
 exports.registerListener = async (req, res) => {
     try {
         const { callback } = req.body;
@@ -50,7 +22,6 @@ exports.registerListener = async (req, res) => {
     }
 };
 
-// Unregister a listener
 exports.unregisterListener = async (req, res) => {
     try {
         const { id } = req.params;
--- Services/EventPublisher.js
+++ Services/EventPublisher.js
@@ -1,35 +1,6 @@
-// const axios = require('axios');
-// const EventHub = require('../Models/EventHub');
-//
-// const buildEvent = (type, customer) => ({
-//     "@type": type,
-//     eventId: `${Date.now()}-${Math.random()}`,
-//     eventTime: new Date().toISOString(),
-//     eventType: type,
-//     event: { customer }
-// });
-//
-// exports.publishEvent = async (type, customer) => {
-//     const hubs = await EventHub.find();
-//     const event = buildEvent(type, customer);
-//
-//     for (const hub of hubs) {
-//         try {
-//             await axios.post(hub.callback, event, {
-//                 headers: { 'Content-Type': 'application/json' }
-//             });
-//         } catch (err) {
-//             console.error(`Failed to notify ${hub.callback}:`, err.message);
-//         }
-//     }
-// };
-
-
-// services/notificationService.js
 const axios = require('axios');
 const EventHub = require('../Models/EventHub');
 
-// Build event in TMF-compatible format
 const buildEvent = (type, customer) => ({
     "@type": type,
     eventId: `${Date.now()}-${Math.random().toString(36).substring(2, 8)}`,
@@ -40,7 +11,6 @@ const buildEvent = (type, customer) => ({
     }
 });
 
-// Publish event to all registered listeners
 exports.publishEvent = async (type, customer) => {
     const hubs = await EventHub.find();
     if (hubs.length === 0) {
